88d900790ad82d63450e766edca98e2ffbd2fe52
Gislayne Vieira <124613642+GIdesigner@users.noreply.github.com>
2023-02-15 13:42:02 -0300

    Add files via upload

---
+++ EXE03-script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('contact-form');
+form.addEventListener('submit', (event) => {event.preventDefault()});
+
+const name = form.elements ['name'].value;
+const email = form.elements ['email'].value;
+const message = form.elemnts ['message'].value;
+
+const result = 'Nome: ${name}\nE-mail: ${email}\nMessage: ${message}';
+alert(result);
